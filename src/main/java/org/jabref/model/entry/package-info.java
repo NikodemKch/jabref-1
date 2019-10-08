0efe4394bb6eb296fe4f2789ccf1266c7b6c7881
Guenes <guenes.aydin@stud.unibas.ch>
2019-10-08 14:01:18 +0200

    Add package info to entry-package

--- src/main/java/org/jabref/model/entry/package-info.java
+++ src/main/java/org/jabref/model/entry/package-info.java
@@ -1,5 +1,5 @@
 /**
- *
+ * This package will store all information from the user that made an entry and split in different categories. Also it contains a converter to BibTeX or BibLateX.
  */
 
 package org.jabref.model.entry;
