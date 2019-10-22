bee84109f4b4b6dc4de3b23d21356ad2fe2583c1
Güni <guenes.aydin@stud.unibas.ch>
2019-10-22 18:56:18 +0200

    Edit SilverBulletHandler. It will now look if UselessFeature is active to add the silverbullet paper.

--- src/main/java/org/jabref/gui/silverbullet/SilverBulletHandler.java
+++ src/main/java/org/jabref/gui/silverbullet/SilverBulletHandler.java
@@ -1,11 +1,13 @@
 package org.jabref.gui.silverbullet;
 
 import com.google.common.eventbus.Subscribe;
+import org.jabref.Globals;
 import org.jabref.gui.DialogService;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.model.database.event.EntryAddedEvent;
 import org.jabref.model.entry.event.EntryChangedEvent;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.preferences.JabRefPreferences;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,16 +31,18 @@ public class SilverBulletHandler {
     //System.out.println("Entry Added");
   }
   @Subscribe
-  public void listen(EntryChangedEvent changedEvent){
-    if (changedEvent.getBibEntry().getField(StandardField.TITLE).isPresent()){
-
+  public void listen(EntryChangedEvent changedEvent) {
+    if (changedEvent.getBibEntry().getField(StandardField.TITLE).isPresent()) {
 
       LOGGER.info(changedEvent.getBibEntry().getField(StandardField.TITLE).get());
       //System.out.println(changedEvent.getBibEntry().getField(StandardField.TITLE).get());
     }
-    if(changedEvent.getBibEntry().getTitle().get().equals("no silver bullet")){
-      dialogService.showInformationDialogAndWait(Localization.lang("no_silver_bullet"),Localization.lang("no_silver_bullet_content"));
+    if (Globals.prefs.getBoolean(JabRefPreferences.USE_USELESS_FEATURE)) {
+      if (changedEvent.getBibEntry().getTitle().get().equals("no silver bullet")) {
+        dialogService.showInformationDialogAndWait(Localization.lang("no_silver_bullet"),
+            Localization.lang("no_silver_bullet_content"));
+
+      }
     }
   }
-
 }
