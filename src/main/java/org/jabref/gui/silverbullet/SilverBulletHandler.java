540cb8694a9a8a4b7d7e8fcb95a7a838c26ff7a9
Güni <guenes.aydin@stud.unibas.ch>
2019-10-22 11:50:44 +0200

    Change System.outs with Logger messages

--- src/main/java/org/jabref/gui/silverbullet/SilverBulletHandler.java
+++ src/main/java/org/jabref/gui/silverbullet/SilverBulletHandler.java
@@ -1,22 +1,28 @@
 package org.jabref.gui.silverbullet;
 
-import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import org.jabref.model.database.event.EntryAddedEvent;
-
 import org.jabref.model.entry.event.EntryChangedEvent;
 import org.jabref.model.entry.field.StandardField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SilverBulletHandler {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(SilverBulletHandler.class);
+
   @Subscribe
   public void listen(EntryAddedEvent addedEvent){
-    System.out.println("Entry Added");
+    LOGGER.info("Entry Added");
+
+    //System.out.println("Entry Added");
   }
   @Subscribe
   public void listen(EntryChangedEvent changedEvent){
     if (changedEvent.getBibEntry().getField(StandardField.TITLE).isPresent()){
-      System.out.println(changedEvent.getBibEntry().getField(StandardField.TITLE).get());
+
+      LOGGER.info(changedEvent.getBibEntry().getField(StandardField.TITLE).get());
+      //System.out.println(changedEvent.getBibEntry().getField(StandardField.TITLE).get());
     }
   }
 }
