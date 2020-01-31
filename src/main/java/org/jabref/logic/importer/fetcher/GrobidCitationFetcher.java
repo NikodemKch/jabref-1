deb65c000c8f6b1ff1b362d265f5fedfec721671
nikch <nikodem.kernbach@stud.unibas.ch>
2020-01-31 17:49:00 +0100

    Fixed two of the discussion issues

--- src/main/java/org/jabref/logic/importer/fetcher/GrobidCitationFetcher.java
+++ src/main/java/org/jabref/logic/importer/fetcher/GrobidCitationFetcher.java
@@ -23,7 +23,6 @@ public class GrobidCitationFetcher implements SearchBasedFetcher {
     private ImportFormatPreferences importFormatPreferences;
     private FileUpdateMonitor fileUpdateMonitor;
     private GrobidService grobidService;
-    private List<String> failedEntries = new ArrayList<>();
 
     public GrobidCitationFetcher(ImportFormatPreferences importFormatPreferences, FileUpdateMonitor fileUpdateMonitor, GrobidService grobidService) {
       this.importFormatPreferences = importFormatPreferences;
@@ -41,34 +40,34 @@ public class GrobidCitationFetcher implements SearchBasedFetcher {
      * Passes request to grobid server, using consolidateCitations option to improve result.
      * Takes a while, since the server has to look up the entry.
      */
-    private String parseUsingGrobid(String plainText) throws FetcherException {
+    private String parseUsingGrobid(String plainText) {
         try {
             return grobidService.processCitation(plainText, GrobidService.ConsolidateCitations.WITH_METADATA);
         } catch (GrobidServiceException e) {
-            throw new FetcherException("The Pipeline failed to get the results from the GROBID client", e);
+            return "";
         }
     }
 
-    private Optional<BibEntry> parseBibToBibEntry(String bibtexString) throws FetcherException {
+    private Optional<BibEntry> parseBibToBibEntry(String bibtexString) {
         try {
             return BibtexParser.singleFromString(bibtexString,
                     importFormatPreferences, fileUpdateMonitor);
         } catch (ParseException e) {
-            throw new FetcherException("Jabref failed to extract a BibEntry form bibtexString.", e);
+            return Optional.empty();
         }
     }
 
     @Override
-    public List<BibEntry> performSearch(String query) throws FetcherException {
+    public List<BibEntry> performSearch(String query) {
         TreeSet<String> plainReferences = Arrays.stream( query.split( "[\\r\\n]+" ) )
               .map(String::trim)
               .collect(Collectors.toCollection(TreeSet::new));
         if (plainReferences.size() == 0) {
-            return List.of();
+            return Collections.emptyList();
         } else {
           List<BibEntry> resultsList = new ArrayList<>();
             for (String reference: plainReferences) {
-                parseBibToBibEntry(parseUsingGrobid(reference)).ifPresentOrElse(resultsList::add, () -> failedEntries.add(reference));
+                parseBibToBibEntry(parseUsingGrobid(reference)).ifPresent(resultsList::add);
             }
             return resultsList;
         }
@@ -77,9 +76,5 @@ public class GrobidCitationFetcher implements SearchBasedFetcher {
     @Override
     public String getName() {
         return "GROBID";
-    }
-
-    public List<String> getFailedEntries() {
-        return failedEntries;
     }
 }
